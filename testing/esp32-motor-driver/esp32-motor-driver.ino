1677ea5f936e465be3d1db95492ab0c8f67a0b81
cosmos5050 <ecosma05@gmail.com>
2026-08-16 13:09:08 -0700

    esp32 to driver to 1 motor test

--- testing/esp32-motor-driver/esp32-motor-driver.ino
+++ testing/esp32-motor-driver/esp32-motor-driver.ino
@@ -1,9 +1,58 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
 
+// I2C pins for esp32
+#define SDA_PIN 21
+#define SCL_PIN 22
+
+// Servo PWM frequency
+#define SERVO_FREQ 50
+
+// PCA9685 12-bit resolution
+#define SERVO_COUNTS 4096
+
+#define SERVO_PERIOD_US (1000000 / SERVO_FREQ)
+
+// Min and max servo pulse width in us out of 20ms
+#define SERVO_PW_MIN_US 500
+#define SERVO_PW_MAX_US 2500
+
+// Min and max angles
+#define SERVO_ANGLE_MIN 0
+#define SERVO_ANGLE_MAX 180
+
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(0x40);
+
+// Wrapper function to control angles instead of counts
+void setServoAngle(uint8_t channel, int angle) {
+  int pulse_us = map(
+    angle,
+    SERVO_ANGLE_MIN,
+    SERVO_ANGLE_MAX,
+    SERVO_PW_MIN_US,
+    SERVO_PW_MAX_US
+  );
+
+  // Angle to counts conversion
+  int pulse_count = (pulse_us * SERVO_COUNTS) / SERVO_PERIOD_US;
+
+  pwm.setPWM(channel, 0, pulse_count);
+}
+
+void setup() {
+  Wire.begin(SDA_PIN, SCL_PIN);
+
+  pwm.begin();
+  pwm.setPWMFreq(SERVO_FREQ);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  setServoAngle(0, 0);
+  delay(1000);
 
+  setServoAngle(0, 90);
+  delay(1000);
+
+  setServoAngle(0, 180);
+  delay(1000);
 }
